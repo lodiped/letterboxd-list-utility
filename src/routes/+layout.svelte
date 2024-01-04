b06d59a6815ac1e0bfdfac5c405629f74fdf2bdf
lodiped <lodiped@gmail.com>
2024-01-04 13:52:50 -0300

    Ui work and detailing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
-    import "../app.css"
+	import '../app.css';
 </script>
+
+<header class="flex w-full justify-center items-center gap-10 py-4 bg-background">
+	<div class="flex items-center cursor-pointer">
+		<div class="flex mr-2">
+			<div class="bg-orange-500 h-6 w-6 rounded-full"></div>
+			<div class="bg-green-500 h-6 w-6 rounded-full"></div>
+			<div class="bg-blue-500 h-6 w-6 rounded-full"></div>
+		</div>
+		<h1 class="text-3xl text-white font-[Inter] font-bold tracking-tight">Lists++</h1>
+	</div>
+	<div class="flex gap-5 text-xs tracking-widest">
+		<span class="text-greytext font-bold cursor-pointer hover:text-white">FILMS</span>
+		<span class="text-greytext font-bold cursor-pointer hover:text-white">LISTS</span>
+		<span class="text-greytext font-bold cursor-pointer hover:text-white">POLLS</span>
+	</div>
+</header>
 
 <slot />
